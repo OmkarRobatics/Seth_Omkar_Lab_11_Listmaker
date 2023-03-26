0fbbec7abf341108880f199bae3c7b852c8e0d44
OmkarRobatics <omkarseth008@gmail.com>
2023-03-26 12:47:53 -0400

    Project init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final String menu = "A - Add D - Delete P - Print Q - Quit";
+        boolean done = false;
+        String command = "";
+        Scanner in = new Scanner(System.in);
+        do
+        {
+            command = 
+        }while (!done)
     }
 }
